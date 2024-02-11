99664faf85e12c72a3a98a10834a9c761b702d72
Jake <jake@cocowireless.org>
2024-02-11 12:09:04 -1000

    cleaned up a little

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,9 +3,6 @@ import os
 from pinecone import Pinecone, ServerlessSpec
 import numpy as np
 
-PINECONE_KEY="a6bc829e-961f-4cda-b8cb-bf283cf6b032"
-OPENAI_KEY = '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'
-# print("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" == OPENAI_KEY)
 openai.api_key = OPENAI_KEY
 
 # openai.api_key = os.getenv('OPENAI_KEY')
